78d70c313d1dcb24f2718458bf386540ddf43c4f
Tnhu:)) <nhutran.31241023442@st.ueh.edu.vn>
2026-02-25 13:27:21 +0700

    Update sp_GetActorsByMovie.sql

--- docs/procedure/procedure_tuyetnhu2006/sp_GetActorsByMovie.sql
+++ docs/procedure/procedure_tuyetnhu2006/sp_GetActorsByMovie.sql
@@ -1,14 +1,17 @@
 DELIMITER $$
-CREATE DEFINER=`root`@`localhost` PROCEDURE `sp_GetActorsByMovie`(IN p_MovieID INT)
+CREATE DEFINER=`root`@`localhost` PROCEDURE `sp_GetActorsByMovie`(
+    IN p_MovieID INT
+)
 BEGIN
-    SELECT 
-        a.Actor_ID,
-        a.Actor_Name,
-        a.Actor_Info,
-        a.Actor_Social
+    SELECT a.Actor_ID,
+           a.Actor_Name,
+           a.Actor_Info,
+           a.Actor_Social
     FROM tbl_actor a
-    INNER JOIN tbl_movie_actor ma 
-        ON a.Actor_ID = ma.Actor_ID
-    WHERE ma.Movie_ID = p_MovieID;
+    INNER JOIN tbl_character c 
+        ON a.Actor_ID = c.Actor_ID
+    INNER JOIN tbl_movie m 
+        ON c.Movie_ID = m.Movie_ID
+    WHERE m.Movie_ID = p_MovieID;
 END$$
 DELIMITER ;
