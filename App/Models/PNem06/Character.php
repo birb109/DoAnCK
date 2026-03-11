0a9d1e990bcb0afd6ed99c84e738fa436f1cd3b4
PNem06 <ptphuongnam06@gmail.com>
2026-03-11 07:59:39 +0700

    Add Character class with methods for character data

--- App/Models/PNem06/Character.php
+++ App/Models/PNem06/Character.php
@@ -1,1 +1,52 @@
+<?php
 
+class Character {
+
+    private $conn;
+
+    private $id;
+    private $name;
+
+    public function __construct($conn){
+        $this->conn = $conn;
+    }
+
+    public function setCharacter($id,$name){
+        $this->id = $id;
+        $this->name = $name;
+    }
+
+    public function getId(){
+        return $this->id;
+    }
+
+    public function getName(){
+        return $this->name;
+    }
+
+    // UML method
+    public function getCharInfo($char_id){
+
+        $sql = "
+        SELECT 
+            c.name AS character_name,
+            a.name AS actor_name,
+            m.title AS movie_title
+        FROM character c
+        JOIN actor a ON c.actor_id = a.id
+        JOIN movie m ON c.movie_id = m.id
+        WHERE c.id = ?
+        ";
+
+        $stmt = $this->conn->prepare($sql);
+        $stmt->bind_param("i",$char_id);
+        $stmt->execute();
+
+        $result = $stmt->get_result();
+
+        return $result->fetch_assoc();
+    }
+
+}
+
+?>
