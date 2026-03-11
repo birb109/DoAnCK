a1695d328658782756e8c1b0d8a73b5b4160677c
PNem06 <ptphuongnam06@gmail.com>
2026-03-11 08:09:01 +0700

    Refactor Actor.php to improve code readability
    Removed unnecessary whitespace and comments for cleaner code.

--- App/Models/PNem06/Actor.php
+++ App/Models/PNem06/Actor.php
@@ -1,36 +1,26 @@
 <?php
-
 class Actor {
-
     private $conn;
-
     private $id;
     private $name;
     private $info;
-
     public function __construct($conn){
         $this->conn = $conn;
     }
-
     public function setActor($id,$name,$info){
         $this->id = $id;
         $this->name = $name;
         $this->info = $info;
     }
-
     public function getId(){
         return $this->id;
     }
-
     public function getName(){
         return $this->name;
     }
-
     public function getInfo(){
         return $this->info;
     }
-
-    // UML method
     public function getCharacters($actor_id){
 
         $sql = "
@@ -48,16 +38,11 @@ class Actor {
         $stmt->execute();
 
         $result = $stmt->get_result();
-
         $characters = [];
-
         while($row = $result->fetch_assoc()){
             $characters[] = $row;
         }
-
         return $characters;
     }
-
 }
-
 ?>
